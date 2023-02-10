24418fb7734b81e970ed9cf220798db38d411db0
kondoumh <kondoumh@gmail.com>
2023-02-10 09:05:28 +0900

    apply composition api

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -3,207 +3,206 @@
 </template>
 
 <script setup>
+  import { computed, ref, onMounted } from 'vue';
+  import * as d3 from 'd3'
+
+  const graphData = ref([]);
+  const width = ref(0);
+  const height = ref(0);
+
   const props = defineProps({
     project: String,
-  })
-</script>
+  });
 
-<script>
-import * as d3 from 'd3'
-
-export default {
-  name: 'graph',
-  computed: {
-    nodes() {
-      let nodes = this.graphData.pages
+  const nodes = computed(() => {
+    let nodes = graphData.value.pages
       .map(page =>
       ({
         id: page.id,
         title: page.title,
-        x: this.width * Math.random(),
-        y: this.height * Math.random(),
-        rx: this.byteLength(page.title) * 2,
+        x: width.value * Math.random(),
+        y: height.value * Math.random(),
+        rx: byteLength(page.title) * 2,
         ry: 10,
         user: false
-      }))
+      }));
 
-      const users = this.graphData.users.map(user => ({
+      const users = graphData.value.users.map(user => ({
         id: user.id,
         title: user.name,
-        x: this.width * Math.random(),
-        y: this.height * Math.random(),
-        rx: this.byteLength(user.name),
+        x: width.value * Math.random(),
+        y: height.value * Math.random(),
+        rx: byteLength(user.name),
         ry: 10,
         user: true
-      }))
-      nodes = nodes.concat(users)
-      return nodes
-    },
-    edges() {
-      const ids = new Set(this.nodes.map(node => node.id))
+      }));
+      nodes = nodes.concat(users);
+      return nodes;
+  });
+
+  const edges = computed(() => {
+    const ids = new Set(nodes.value.map(node => node.id))
       const idm = new Map()
-      this.nodes.forEach((node, index) => {idm[node.id] = index})
-      let edges = this.graphData.links
+      nodes.value.forEach((node, index) => {idm[node.id] = index})
+      let edges = graphData.value.links
         .filter(edge => ids.has(edge.from) && ids.has(edge.to))
         .map(edge =>
         ({
           source: idm[edge.from],
           target: idm[edge.to],
           l: Math.random() * 150
-        }))
+        }));
 
-      const userPages = this.graphData.userPages
+      const userPages = graphData.value.userPages
         .filter(up => ids.has(up.user) && ids.has(up.page))
         .map(up =>
         ({
           source: idm[up.user],
           target: idm[up.page],
           l: Math.random() * 300
-        })) 
-      edges = edges.concat(userPages)
-      return edges
-    }
-  },
-  data: () => ({
-    graphData: [],
-    width: 0,
-    height: 0
-  }),
-  async mounted () {
-    this.width = document.querySelector('svg').clientWidth
-    this.height = document.querySelector('svg').clientHeight
-    await this.fetchData()
-    await this.render()
-  },
-  methods: {
-    async fetchData() {
-      const res = await fetch(`https://sb-graph-kondoumh.netlify.app/${encodeURIComponent(this.project)}_graph.json`, {
-        mode: 'cors'
-      })
-      this.graphData = await res.json()
-    },
-    async render() {
-      d3.select('svg').selectAll('*').remove()
+        }));
+      edges = edges.concat(userPages);
+      return edges;
+  });
 
-      const zoom = d3.zoom()
-        .scaleExtent([1/3, 40])
-        .on('zoom', zoomed)
+  onMounted( async () => {
+    width.value = document.querySelector('svg').clientWidth;
+    height.value = document.querySelector('svg').clientHeight;
+    await fetchData();
+    await render();
+  });
 
-      d3.select('svg')
-        .attr('viewBox', '0 0 1200 1400')
-        .attr("preserveAspectRatio", "xMidYMid meet")
-        .call(zoom)
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://sb-graph-kondoumh.netlify.app/${encodeURIComponent(props.project)}_graph.json`, 
+      { mode: 'cors'}
+    );
+    graphData.value = await res.json();
+  };
 
-      const link = d3.select('svg')
-        .selectAll('line')
-        .data(this.edges)
-        .enter()
-        .append('line')
-        .attr('stroke-width', 1)
-        .attr('stroke', 'LightGray')
+  const render = async () => {
+    // d3.select('svg').selectAll('*').remove();
 
-      const nodeGroup = d3.select('svg')
-        .selectAll('g')
-        .data(this.nodes)
-        .enter()
-        .append('g')
-        .call(d3.drag()
-          .on('start', dragstarted)
-          .on('drag', dragged)
-          .on('end', dragended))
-        .on('click', this.openPage)
+    const zoom = d3.zoom()
+      .scaleExtent([1/3, 40])
+      .on('zoom', zoomed);
 
-      nodeGroup.append('ellipse')
+    d3.select('svg')
+      .attr('viewBox', '0 0 1200 1400')
+      .attr("preserveAspectRatio", "xMidYMid meet")
+      .call(zoom);
+
+    const link = d3.select('svg')
+      .selectAll('line')
+      .data(edges.value)
+      .enter()
+      .append('line')
+      .attr('stroke-width', 1)
+      .attr('stroke', 'LightGray');
+
+    const nodeGroup = d3.select('svg')
+      .selectAll('g')
+      .data(nodes.value)
+      .enter()
+      .append('g')
+      .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended))
+      .on('click', openPage);
+
+    nodeGroup.append('ellipse')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('rx', d => d.rx)
+      .attr('ry', d => d.ry)
+      .attr('fill', d => d.user ? 'Cyan' : 'Gold');
+
+    nodeGroup.append('text')
+      .attr('x', d => d.x)
+      .attr('y', d => d.y)
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'middle')
+      .style('fill', 'steelbule')
+      .style('font-size', '11px')
+      .text(d => d.title);
+
+    const simulation = d3.forceSimulation()
+      .force('link',
+        d3.forceLink()
+          .distance(d => d.l)
+          .iterations(2))
+      .force('collide',
+        d3.forceCollide()
+          .radius(d => d.r)
+          .strength(0.7)
+          .iterations(2))
+      .force('charge', d3.forceManyBody().strength(-100))
+      .force('x', d3.forceX().strength(0.01).x(width.value / 2))
+      .force('y', d3.forceY().strength(0.01).y(height.value / 2))
+      .force('center', d3.forceCenter(width.value / 2, height.value / 2));
+
+    simulation
+      .nodes(nodes.value)
+      .on('tick', ticked);
+
+    simulation.force('link')
+      .links(edges.value)
+      .id(d => d.index);
+
+    function ticked() {
+      link
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y);
+      nodeGroup.select('ellipse')
         .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr('rx', d => d.rx)
-        .attr('ry', d => d.ry)
-        .attr('fill', d => d.user ? 'Cyan' : 'Gold')
-
-      nodeGroup.append('text')
+        .attr('cy', d => d.y);
+      nodeGroup.select('text')
         .attr('x', d => d.x)
-        .attr('y', d => d.y)
-        .attr('text-anchor', 'middle')
-        .attr('dominant-baseline', 'middle')
-        .style('fill', 'steelbule')
-        .style('font-size', '11px')
-        .text(d => d.title)
-
-      const simulation = d3.forceSimulation()
-        .force('link',
-          d3.forceLink()
-            .distance(d => d.l)
-            .iterations(2))
-        .force('collide',
-          d3.forceCollide()
-            .radius(d => d.r)
-            .strength(0.7)
-            .iterations(2))
-        .force('charge', d3.forceManyBody().strength(-100))
-        .force('x', d3.forceX().strength(0.01).x(this.width / 2))
-        .force('y', d3.forceY().strength(0.01).y(this.height / 2))
-        .force('center', d3.forceCenter(this.width / 2, this.height / 2))
-
-      simulation
-        .nodes(this.nodes)
-        .on('tick', ticked)
-
-      simulation.force('link')
-        .links(this.edges)
-        .id(d => d.index)
-
-      function ticked() {
-        link
-          .attr('x1', d => d.source.x)
-          .attr('y1', d => d.source.y)
-          .attr('x2', d => d.target.x)
-          .attr('y2', d => d.target.y)
-        nodeGroup.select('ellipse')
-          .attr('cx', d => d.x)
-          .attr('cy', d => d.y)
-        nodeGroup.select('text')
-          .attr('x', d => d.x)
-          .attr('y', d => d.y)
-      }
-
-      function dragstarted(e, d) {
-        if (!e.active) simulation.alphaTarget(0.3).restart()
-        d.fx = d.x
-        d.fy = d.y
-      }
-
-      function dragged(e, d) {
-        d.fx = e.x
-        d.fy = e.y
-      }
-
-      function dragended(e, d) {
-        if (!e.active) simulation.alphaTarget(0)
-        d.fx = null
-        d.fy = null
-      }
-
-      link.call(zoom)
-      nodeGroup.call(zoom)
-    
-      function zoomed(e, d) {
-        link.attr('transform', e.transform)
-        nodeGroup.attr('transform', e.transform)
-      }
-    },
-    byteLength(str) {
-      str = (str==null) ? "" : str
-      return encodeURI(str).replace(/%../g, "*").length
-    },
-    openPage(e, d) {
-      console.log(d)
-      if (d.user) return
-      const page = encodeURIComponent(d.title)
-      const url = `https://scrapbox.io/${encodeURIComponent(this.project)}/${page}`
-      window.open(url)
+        .attr('y', d => d.y);
     }
-  }
-}
+
+    function dragstarted(e, d) {
+      if (!e.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(e, d) {
+      d.fx = e.x;
+      d.fy = e.y;
+    }
+
+    function dragended(e, d) {
+      if (!e.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+
+    link.call(zoom)
+    nodeGroup.call(zoom)
+
+    function zoomed(e, d) {
+      link.attr('transform', e.transform);
+      nodeGroup.attr('transform', e.transform);
+    }
+  };
+
+  const byteLength = (str) => {
+    str = (str==null) ? "" : str;
+    return encodeURI(str).replace(/%../g, "*").length;
+  };
+
+  const openPage = (e, d) => {
+    console.log(d);
+    if (d.user) return;
+    const page = encodeURIComponent(d.title);
+    const url = `https://scrapbox.io/${encodeURIComponent(props.project)}/${page}`;
+    window.open(url);
+  };
+
 </script>
 
 <style scoped>
